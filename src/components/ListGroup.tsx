6ebebd542d714636174fe636c83e0758f823f1bb
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-07-16 20:46:22 +0400

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,6 +4,7 @@ function ListGroup() {
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">

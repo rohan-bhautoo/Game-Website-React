2d5a6bf48b1bee183de4b775840c0f40566cfa2d
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-07-16 18:38:13 +0400

    Added bootstrap + ListGroup component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">1</li>
+      <li className="list-group-item">2</li>
+      <li className="list-group-item">3</li>
+      <li className="list-group-item">4</li>
+      <li className="list-group-item">5</li>
+    </ul>
+  );
+}
+
+export default ListGroup;

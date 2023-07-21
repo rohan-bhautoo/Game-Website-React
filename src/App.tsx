8189d4e4051fcb3d322114a6f4f218937f236d31
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-07-21 20:50:39 +0400

    Passing Functions via Props

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./components/ListGroup";
 function App() {
   const items = ["Mauritius", "Canada", "England", "South Africa"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities"></ListGroup>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
     </div>
   );
 }

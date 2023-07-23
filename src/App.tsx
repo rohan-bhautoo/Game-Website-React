1a8392d798dd4f518c3f89188340642beb8db6d3
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-07-23 14:07:32 +0400

    CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  const items = ["Mauritius", "Canada", "England", "South Africa"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
       <Alert>
         Hello <span>World</span>
       </Alert>

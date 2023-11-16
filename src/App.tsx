efd86d9761689919ee21ffc665ab1ad6c91f996f
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-11-16 13:35:47 +0400

    State Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   const items = ["Mauritius", "Canada", "England", "South Africa"];
+  const [isVisible, setVisibility] = useState(false);
+  const [isApproved, setApproved] = useState(true);
+  let count = 0;
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
+  const handleSelectItem = () => {
+    setVisibility(true);
+    count++;
+    console.log(isVisible);
   };
 
   return (

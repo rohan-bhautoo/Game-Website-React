8e829cbf1cb90849ba23db3fe17b5b57afce0c80
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-11-16 13:40:27 +0400

    State Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
 
 function App() {
-  const items = ["Mauritius", "Canada", "England", "South Africa"];
-  const [isVisible, setVisibility] = useState(false);
-  const [isApproved, setApproved] = useState(true);
-  let count = 0;
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+  });
+  const [isLoading, setLoading] = useState(false);
 
-  const handleSelectItem = () => {
-    setVisibility(true);
-    count++;
-    console.log(isVisible);
-  };
-
-  return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

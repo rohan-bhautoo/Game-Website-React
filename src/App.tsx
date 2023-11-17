90b09463f66ab20281508468b16ae9731baa112e
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-11-17 12:30:01 +0400

    Updating Arrays

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import Message from "./Message";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
+  const [tags, setTags] = useState(["happy", "cheerful"]);
 
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    // Add
+    setTags([...tags, "sad"]);
+
+    // Remove
+    setTags(tags.filter((tag) => tag != "happy"));
+
+    // Update
+    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
   };
 
   return (

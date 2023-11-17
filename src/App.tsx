b9fa1c848846e3b7d6e3c3bd55d88dd03ae83477
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-11-17 20:46:12 +0400

    Update Logic With Immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
-import Message from "./Message";
+import { produce } from "immer";
 
 function App() {
-  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    // Add
-    setTags([...tags, "sad"]);
+    //setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
 
-    // Remove
-    setTags(tags.filter((tag) => tag != "happy"));
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
 
-    // Update
-    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+        if (bug) {
+          bug.fixed = true;
+        }
+      })
+    );
   };
 
   return (
-    <>
-      <Message></Message>
-      <Message></Message>
-    </>
+    <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
+      <button onClick={handleClick}>Click Me</button>
+    </div>
   );
 }
 

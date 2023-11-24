532b23d6a4a57e18d15a0559528fdfa31d62c71b
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-11-24 17:15:33 +0400

    Critic Score

---
+++ src/components/CriticScrore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScrore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={1} borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScrore;

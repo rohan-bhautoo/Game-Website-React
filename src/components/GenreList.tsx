bb1a9aa69adf149b49cbe88c3f4c62dc0b1fc9d6
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-11-26 11:32:00 +0400

    Refactoring Query Object

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -36,6 +36,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
               variant="link"
+              overflow={"auto"}
             >
               {genre.name}
             </Button>

96d2a65225b6eea8f8d2b0cad062feec27d31a53
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-11-27 20:04:48 +0400

    Filtered Platforms List

--- src/components/PlatformList.tsx
+++ src/components/PlatformList.tsx
@@ -33,47 +33,49 @@ const PlaformList = ({ onSelectPlatform, selectedPlatform }: Props) => {
         Platforms
       </Heading>
       <List>
-        {data.map((platform) => (
-          <ListItem key={platform.id} paddingY="5px">
-            <HStack>
-              <IconButton
-                key={platform.id}
-                onClick={() => onSelectPlatform(platform)}
-                boxSize="32px"
-                borderRadius={8}
-                bgColor={
-                  platform.id === selectedPlatform?.id ? "white" : "gray.700"
-                }
-                objectFit={"cover"}
-                icon={
-                  <Icon
-                    key={platform.id}
-                    as={iconMap[platform.slug]}
-                    color={
-                      platform.id === selectedPlatform?.id
-                        ? "black"
-                        : "gray.500"
-                    }
-                  />
-                }
-                aria-label={platform.slug}
-              />
-              <Button
-                fontWeight={
-                  platform.id === selectedPlatform?.id ? "bold" : "normal"
-                }
-                onClick={() => onSelectPlatform(platform)}
-                fontSize="lg"
-                variant="link"
-                whiteSpace={"normal"}
-                justifyContent="flex-start"
-                textAlign={"left"}
-              >
-                {platform.name}
-              </Button>
-            </HStack>
-          </ListItem>
-        ))}
+        {data
+          .filter((platform) => iconMap.hasOwnProperty(platform.slug))
+          .map((platform) => (
+            <ListItem key={platform.id} paddingY="5px">
+              <HStack>
+                <IconButton
+                  key={platform.id}
+                  onClick={() => onSelectPlatform(platform)}
+                  boxSize="32px"
+                  borderRadius={8}
+                  bgColor={
+                    platform.id === selectedPlatform?.id ? "white" : "gray.700"
+                  }
+                  objectFit={"cover"}
+                  icon={
+                    <Icon
+                      key={platform.id}
+                      as={iconMap[platform.slug]}
+                      color={
+                        platform.id === selectedPlatform?.id
+                          ? "black"
+                          : "gray.500"
+                      }
+                    />
+                  }
+                  aria-label={platform.slug}
+                />
+                <Button
+                  fontWeight={
+                    platform.id === selectedPlatform?.id ? "bold" : "normal"
+                  }
+                  onClick={() => onSelectPlatform(platform)}
+                  fontSize="lg"
+                  variant="link"
+                  whiteSpace={"normal"}
+                  justifyContent="flex-start"
+                  textAlign={"left"}
+                >
+                  {platform.name}
+                </Button>
+              </HStack>
+            </ListItem>
+          ))}
       </List>
     </>
   );
